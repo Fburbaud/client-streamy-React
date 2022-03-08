b5e660935554d3f5cfee004157af80d51675eb31
Fburbaud <fred.brechet@gmail.com>
2022-03-08 10:09:57 +0100

    gapi wip

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,9 +1,26 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+    state = { isSignedIn: null };
+
+    componentDidMount() {
+        //wiring up google api library
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '1094188205201-bjqcg95cnlr2frfvmhfben7t17riutff.apps.googleusercontent.com',
+                scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+            });
+        });
+    }
+    
     render() {
         return(
-            <div>google auth</div>
+            <div>
+                google auth
+            </div>
         );
     };
 };
