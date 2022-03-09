0003ea9a28544e19056b2ed217852057f6c147ba
Fburbaud <fred.brechet@gmail.com>
2022-03-09 14:06:13 +0100

    stream create wip

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form"; //as is used to rename the variable so we don't get confused
 import authReducer from "./authReducer";
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
