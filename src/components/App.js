f7284f88e151757da731d6100e5f4459cb7b19c0
Fburbaud <fred.brechet@gmail.com>
2022-06-15 09:16:02 +0200

    app done

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
 import Header from "./Header";
 import history from "../history";
+import '../style2/App.css'
 
 const App = () => {
     return (
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,8 @@
 import { combineReducers } from "redux";
-import { reducer as formReducer } from "redux-form"; //as is used to rename the variable so we don't get confused
 import authReducer from "./authReducer";
 import streamReducer from "./streamReducer";
 
-//const NodeMediaServer = require('node-media-server');
-
 export default combineReducers({
     auth: authReducer,
-    form: formReducer,
     streams: streamReducer
 });
